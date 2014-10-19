5aa603a68a3e18f63a89f2f25d52ebe1cc880ead
sroysf <saptarshi.roy@salesforce.com>
2014-10-18 18:52:43 -0700

    Fixed packaging for AppMain

--- src/main/java/us/sroysf/AppMain.java
+++ src/main/java/us/sroysf/AppMain.java
@@ -23,7 +23,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-package sroysf;
+package us.sroysf;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
